2519ee4b479851137f492fac73dd25006f194a6f
nayoungkim0 <rachelkim2002@gmail.com>
2023-11-18 19:41:33 -0800

    website formatted

--- myscripts.js
+++ myscripts.js
@@ -1,4 +1,13 @@
-let login = true; 
+let computer = new Array(
+    "A1","A2","A3","A4","A5",
+    "B1","B2","B3","B4","B5",
+    "C1","C2","C3","C4","C5","C6",
+    "D1","D2","D3","D4","D5","D6",
+    "E1","E2","E3","E4"
+);
+
+let login = new Array(26).fill(false); 
+login[3] = true;
 
 var today = new Date();
 const dateString = new Date().toLocaleString([],{
@@ -16,23 +25,30 @@ function refreshTime(){
     timeDisplay.textContent = dateString;
 }
 setInterval(refreshTime, 1000);
-const alldiv = document.querySelectorAll("div > div");
-    let i;
-if(login == true){
-    document.getElementById("status").innerHTML = "reserved";
-   // document.getElementsById("status").style.color = "red";
-    document.getElementById("status").style.color = "red";
-    //document.querySelector("div").style.background = "green";
 
-    for (i=0; i < alldiv.length; i++){
-        alldiv[i].style.background = "red";
-    }
-   
-}
-else{
-    document.getElementById("status").innerHTML = "empty";
-    for (i=0; i < alldiv.length; i++){
-        alldiv[i].style.background = "green";
-    }
+
+const alldiv = document.querySelectorAll(".content");
+let i;
+for (i=0; i < alldiv.length; i++){
+    alldiv[i].style.background = "#E4D5B6"; //cream
 }
 
+for (let j = 0; j <= computer.length; j++){
+    let dot = ".";
+    let pc = computer[j];
+    let whichpc = dot.concat(pc);
+    let time = dot.concat("T");
+    time = time.concat(pc);
+    document.querySelector(whichpc).style.fontWeight = "normal";
+    document.querySelector(time).style.fontWeight = "normal";
+
+    if (login[j] == true){ //logged in
+        document.querySelector(whichpc).innerHTML =  "<br> reserved";
+        document.querySelector(whichpc).style.color = "#F0461A"; //red
+        document.querySelector(time).innerHTML = "time";
+    }
+    else{ //empty
+        document.querySelector(whichpc).innerHTML = "<br> empty";
+        document.querySelector(time).innerHTML = "<br>";
+    }
+}
